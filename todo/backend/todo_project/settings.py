1b78d3ed34448a2b7f36580575053f4bd450cb94
Mohanad Abu-Nayla <aljaboori@gmail.com>
2018-11-20 23:37:22 +0800

    Corsheaders install and setup

--- todo/backend/todo_project/settings.py
+++ todo/backend/todo_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3d party apps
     "rest_framework",
+    'corsheaders',
 
 
     # local
@@ -47,6 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +132,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ]
 }
+
+CORS_ORIGIN_WHITELIST = ('localhost:3000')
